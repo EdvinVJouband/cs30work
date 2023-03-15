31aa728e2098467513799626c4a77469222cdd66
EdvinVJouband <126730421+EdvinVJouband@users.noreply.github.com>
2023-03-15 15:18:50 -0600

    fixed files and stared working on calculating angles

--- array-assignment/sketch.js
+++ array-assignment/sketch.js
@@ -13,16 +13,29 @@ function setup() {
 function draw() {
   background(220);
   drawShape();
-  rect(100, 100, 100);
 }
 
 function drawShape() {
-  beginShape();
-  vertex(100, 100);
-  vertex(50, 50);
-  vertex(100, 50);
-  vertex(50, 100);
-  endShape();
-  circle(100, 100, 100);
+  let diameter = 200;
+  circle(width/2, height/2, diameter);
+  strokeWeight(5);
+  point(width/2, height/2);
+  strokeWeight(1);
+  let y = 100 * cos(45);
+  console.log(y);
+  line(width/2, height/2, width/2 + y, height/2);
 }
+
+// beginShape();
+// vertex(100, 100);
+// vertex(50, 50);
+// vertex(100, 50);
+// vertex(50, 100);
+// endShape();
+
+// let a = 0.0;
+// let inc = TWO_PI / 25.0;
+// for (let i = 0; i < 25; i++) {
+//   line(i * 4, 50, i * 4, 50 + cos(a) * 40.0);
+//   a = a + inc;
 
