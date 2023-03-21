4e9b849c0baa46810f4ed9d10f81613118690a55
EdvinVJouband <126730421+EdvinVJouband@users.noreply.github.com>
2023-03-20 20:05:52 -0600

    finished array assignment

--- array-assignment/sketch.js
+++ array-assignment/sketch.js
@@ -1,41 +1,49 @@
 // Arrays and Object Botation Assignment
 // Edvin Jouband
 // Wednesday March 8, 2023
-//
+// Warning flashing lights !!!
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// I used the cos and sin function to help calculate points at the outside of a circle based on an iterating angle.
 
+// Create array to store the Points object
 let circles = [];
 
+// Make variables for for the diameter and radius of a circle.
 let diameter = 200;
 let radius = diameter/2;
 
+// Create the canvas, change the angle mode to degrees for circle calculations and iterate on the createPoints function to create several objects.
 function setup() {
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
-  for (let i = 0; i < 50; i ++) {
-    displayPoints(width/2, height/2, i);
+  for (let i = 0; i < 500; i ++) {
+    createPoints(width/2, height/2, i);
   }
 }
 
+// Displays the points 60 times a second without refreshing the backrounf so the points stay.
 function draw() {
-  //background(220);
-  calculatePoints();
+  displayPoints();
 }
 
-function calculatePoints() {
+// Calculates points around a circle with SOH CAH TOA and draws the points with random changing colors.
+function displayPoints() {
   let angle = 0;
-    for (let i = 0; i < 24; i ++) {
+  for (let i = 0; i < 500; i ++) {
+    for (let j = 0; j < 24; j ++) {
       strokeWeight(5);
       let adjacentSide = radius * cos(angle);
       let opositeSide = radius * sin(angle);
-      point(width/2 + adjacentSide + random(a*2), height/2 - opositeSide + random(a*2));
+      point(circles[i].x + adjacentSide + random(-circles[i].randomFactor, circles[i].randomFactor + 50), circles[i].y - opositeSide + random(-circles[i].randomFactor, circles[i].randomFactor + 50));
       strokeWeight(1); 
-      angle += 360/24;
+      angle += 360/24; 
+      }
     }
+    stroke(random(0, 255), random(0, 255), random(0, 255));
 }
 
-function displayPoints(theX, theY, howRandom) {
+// Creates an object called Points wich stores an x and y value.
+function createPoints(theX, theY, howRandom) {
   let Points = {
     x: theX,
     y: theY,
@@ -43,57 +51,3 @@ function displayPoints(theX, theY, howRandom) {
   }
   circles.push(Points);
 }
-
-// function displayPoints(theAngle) {
-//   let points = {
-//     angle: theAngle
-//   };
-//   circles.push(points);
-// }
-
-// function calculatePoints() {
-//   let angle = 0;
-//   for (let i = 0; i < 24; i ++) {
-//     strokeWeight(5);
-//     let adjacentSide = radius * cos(angle);
-//     let opositeSide = radius * sin(angle);
-//     point(width/2 + adjacentSide, height/2 - opositeSide);
-//     console.log(mouseX);
-//     strokeWeight(1); 
-//     angle += 360/24;
-//   }
-// }
-
-// function calculatePoint(angle) {
-//   angleMode(DEGREES);
-//   strokeWeight(5);
-//   let adjacentSide = radius * cos(angle);
-//   let opositeSide = radius * sin(angle);
-//   point(width/2 + adjacentSide, height/2 - opositeSide);
-//   console.log(mouseX);
-//   strokeWeight(1);
-// }
-
-// beginShape();
-// vertex(100, 100);
-// vertex(50, 50);
-// vertex(100, 50);
-// vertex(50, 100);
-// endShape();
-
-// function calculatePoint(angle) {
-//   angleMode(DEGREES);
-//   let adjacentSide = radius * cos(angle);
-//   let opositeSide = radius * sin(angle);
-//   line(width/2, height/2, width/2 + adjacentSide, height/2);
-//   line(width/2 + adjacentSide, height/2, width/2 + adjacentSide, height/2 - opositeSide);
-//   line(width/2, height/2, width/2 + adjacentSide, height/2 - opositeSide);
-//   console.log(mouseX);
-// }
-
-// let a = 0.0;
-// let inc = TWO_PI / 25.0;
-// for (let i = 0; i < 25; i++) {
-//   line(i * 4, 50, i * 4, 50 + cos(a) * 40.0);
-//   a = a + inc;
-
