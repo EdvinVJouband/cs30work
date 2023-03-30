af15858a0514f544d29e0d3f4366e1f12a279ce9
EdvinVJouband <126730421+EdvinVJouband@users.noreply.github.com>
2023-03-30 15:19:50 -0600

    worked on 2D array assignement

--- 2D-array-assignement/sketch.js
+++ 2D-array-assignement/sketch.js
@@ -54,6 +54,8 @@ function draw() {
 
 function storeCellValues() {
   let cell = {
+    theX: 
+    theY: 
     theF: 0,
     theG: 0,
     theH: 0,
@@ -76,58 +78,6 @@ function A_Star() {
 
   for (let i = 0; i < openSet.length; i ++) {
     
-  }
-}
-
-function keyTyped() {
-  if (key === "r") {
-    grid = createRandomGrid(ROWS, COLS);
-  }
-  if (key === "s") { //move down
-    moveCharacter(0, 1);
-  }
-  if (key === "w") { //move up
-    moveCharacter(0, -1);
-  }
-  if (key === "d") { //move right
-    moveCharacter(1, 0);
-  }
-  if (key === "a") { //move left
-    moveCharacter(-1, 0);
-  }
-}
-
-function moveCharacter(x, y) {
-  //sanity check for edge cases
-  if (characterX + x >= 0 && characterX + x < COLS &&
-      characterY + y >= 0 && characterY + y < ROWS) {
-    
-    //check if going to hit wall
-    if (grid[characterY+y][characterX+x] === 0) {
-      let tempX = characterX;
-      let tempY = characterY;
-    
-      characterX += x;
-      characterY += y;
-    
-      //update grid
-      grid[characterY][characterX] = 9;
-      grid[tempY][tempX] = 0;
-    }
-
-  }
-}
-
-function mousePressed() {
-  let x = Math.floor(mouseX/cellSize);
-  let y = Math.floor(mouseY/cellSize);
-
-  if (grid[y][x] === 0) {
-    grid[y][x] = 1;
-  }
-
-  else if (grid[y][x] === 1) {
-    grid[y][x] = 0;
   }
 }
 
@@ -168,3 +118,56 @@ function createRandomGrid(ROWS, COLS) {
   }
   return newGrid;
 }
+
+// function keyTyped() {
+//   if (key === "r") {
+//     grid = createRandomGrid(ROWS, COLS);
+//   }
+//   if (key === "s") { //move down
+//     moveCharacter(0, 1);
+//   }
+//   if (key === "w") { //move up
+//     moveCharacter(0, -1);
+//   }
+//   if (key === "d") { //move right
+//     moveCharacter(1, 0);
+//   }
+//   if (key === "a") { //move left
+//     moveCharacter(-1, 0);
+//   }
+// }
+
+// function moveCharacter(x, y) {
+//   //sanity check for edge cases
+//   if (characterX + x >= 0 && characterX + x < COLS &&
+//       characterY + y >= 0 && characterY + y < ROWS) {
+    
+//     //check if going to hit wall
+//     if (grid[characterY+y][characterX+x] === 0) {
+//       let tempX = characterX;
+//       let tempY = characterY;
+    
+//       characterX += x;
+//       characterY += y;
+    
+//       //update grid
+//       grid[characterY][characterX] = 9;
+//       grid[tempY][tempX] = 0;
+//     }
+
+//   }
+// }
+
+// function mousePressed() {
+//   let x = Math.floor(mouseX/cellSize);
+//   let y = Math.floor(mouseY/cellSize);
+
+//   if (grid[y][x] === 0) {
+//     grid[y][x] = 1;
+//   }
+
+//   else if (grid[y][x] === 1) {
+//     grid[y][x] = 0;
+//   }
+// }
+
