dd69bfb5b9d408e774b84f744a1925e1b597c224
EdvinVJouband <126730421+EdvinVJouband@users.noreply.github.com>
2023-04-16 17:15:50 -0600

    finnished commenting everything

--- 2D-array-assignement/sketch.js
+++ 2D-array-assignement/sketch.js
@@ -1,10 +1,15 @@
 // 2D array assignement
 // Edvin Jouband
 // March 27, 2023
-//  The Coding Train, A* in P5 js, 21 min 21 sec
 // https://www.youtube.com/watch?v=aKYlikFAV4k
 // https://www.youtube.com/watch?v=EaZxUCWAjb0&t=0s
+// https://en.wikipedia.org/wiki/A*_search_algorithm
+// https://www.geeksforgeeks.org/a-search-algorithm/
+// https://en.wikipedia.org/wiki/Taxicab_geometry
+// Extra for experts used the A* pathfinding algorithim
 
+// the openSet will contain all the cells that I want to check to find the path
+// and the closedSet will contain all the cells that have already been checked
 let openSet = [], closedSet = [];
 let start, end;
 const ROWS = 25, COLS = 25;
@@ -12,7 +17,6 @@ let cellSize;
 let grid = new Array(COLS);
 let path = [];
 let nosolution = false;
-let solution = false;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -26,8 +30,13 @@ function setup() {
 
   createGrid();
 
+  // make the start point the top left corner
   start = grid[0][0];
+
+  // make the end point the botom right corner
   end = grid[COLS - 1][ROWS - 1];
+
+  // the starting and end points can not be obstacles
   start.wall = false;
   end.wall = false;
 
@@ -36,14 +45,12 @@ function setup() {
 
 function draw() {
   background(220);
-  A_Star(solution);
-  if (solution === true) {
-    noLoop();
-  }
+  A_Star();
   displayGrid();
   displayCells();
 }
 
+// stores all the nessessairy values for the cells in objects
 class Cell {
   constructor(i, j) {
     this.i = i;
@@ -55,10 +62,14 @@ class Cell {
     this.previous = undefined;
     this.wall = false;
 
+    // there is a 30% chance of a cell being generated as a wall/obstacle
     if (random(1) < 0.3) {
       this.wall = true;
     }
 
+    // creates a function to draw the cells as squares with there colors being based on if they are a wall, a unchecked cell,
+    // a checked cell, a neighbor or the path,
+    // these colors were determined in the display cells function except for the walls.
     this.show = function (cellColor) {
       fill(cellColor);
       if (this.wall) {
@@ -68,6 +79,7 @@ class Cell {
       rect(this.i * cellSize, this.j * cellSize, cellSize - 1, cellSize - 1);
     };
 
+    // calculates the neighbors N E S O of the current cell only if possible
     this.addNeighbors = function(grid) {
       let i = this.i;
       let j = this.j;
@@ -87,6 +99,7 @@ class Cell {
   }
 }
 
+// creates a function to loop through an array backwards to find and delete a certain element if it is in the array.
 function removeFromArray(arr, elt){
   for (let i = arr.length - 1; i >= 0; i --) {
     if (arr[i] === elt) {
@@ -95,39 +108,40 @@ function removeFromArray(arr, elt){
   }
 }
 
+// compares the x and y values of a neighboring cell and the end cell to return there estimated distance
 function heuristic(a, b) {
-  // let d = dist(a.i, a.j, b.i, b.j);
   let d = abs(a.i - b.i) + abs(a.j - b.j);
   return d;
 }
 
 function A_Star() {
+  // if the openSet if not empty, so if there are cells to check then the algorithim will run
   if (openSet.length > 0) {
 
+    // finds the cells with the lowest f value, the f value is the value of the movement cost of a going to a cell wich
+    // is called the g value plus the estimated distance of a cell from the end cell wich is called the h value.
     let winner = 0;
     for (let i = 0; i < openSet.length; i ++) {
       if (openSet[i].f < openSet[winner].f) {
         winner = i;
       }
     }
+    // make the current cell of the path be the winner of the lowest f value/lowest movement cost
     let current = openSet[winner];
 
+    // if the current cell of the path is ever the end cell then we have arrived at the end and we can stop.
     if (current === end) {
-      //find the path
-      path = [];
-      let temp = current;
-      path.push(temp);
-      while (temp.previous) {
-        path.push(temp.previous);
-        temp = temp.previous;
-      }
       noLoop();
       console.log("DONE");
     }
 
+    // remove the old current cell we were checking from the openSet and add it to the closedSet
     removeFromArray(openSet, current);
     closedSet.push(current);
 
+    // check throught all the neightbors, that arent walls, off the grid
+    // and havent been already checked and calculate its g value, wich is it's movement cost,
+    // then add the neighbor with the lowest g value to the openSet
     let neighbors = current.neighbors;
     for (let i = 0; i < neighbors.length; i ++) {
       let neighbor = neighbors[i];
@@ -145,28 +159,28 @@ function A_Star() {
           openSet.push(neighbor);
         }
 
+        // heuristic is what the h value stands for, so it's the estimated distance between a neighboring
+        // cell and the end cell
         neighbor.h = heuristic(neighbor, end);
+
+        // calculate the f value of the neighbor.
         neighbor.f = neighbor.g + neighbor.h;
+
+        // let the cell this neighbor came from be the current cell
         neighbor.previous = current;
-
-
       }
     }
-
-    // keep going
   }
   else {
-    console.log('no solution');
-    nosolution = true;
-    return(true);
-    // noLoop();
-
-    // no solution
+    // if the openSet is empty and we havent gotten to the end then there 
+    // was no way to get to the end so theres no solution.
+    // if this else occurs and the algotithim finds no path we will get an error but I didint
+    // bother making a noLoop because it's functionaly the same, other then to maybe see the final path it tried.
   }
 }
 
 function displayGrid() {
-  //display grid
+  // displays the grid with the values from the show function
   for (let i = 0; i < COLS; i++) {
     for (let j = 0; j < ROWS; j++) {
       grid[i][j].show(color(255));
@@ -176,7 +190,7 @@ function displayGrid() {
 }
 
 function createGrid() {
-  // making 2d array
+  // create a 2D array
   for (let i = 0; i < COLS; i++) {
     grid[i] = new Array(ROWS);
   }
@@ -187,6 +201,7 @@ function createGrid() {
     }
   }
 
+  // find the neighboring cells of the current cell being created
   for (let i = 0; i < COLS; i++) {
     for (let j = 0; j < ROWS; j++) {
       grid[i][j].addNeighbors(grid);
@@ -195,14 +210,17 @@ function createGrid() {
 }
 
 function displayCells() {
-  // set the color of the cells
+  // make all cells that have already been checked red
   for (let i = 0; i < closedSet.length; i ++) {
     closedSet[i].show(color(255, 0, 0));
   }
+  // make the color of the neighbors green
   for (let i = 0; i < openSet.length; i ++) {
     openSet[i].show(color(0, 255, 0));
   }
 
+// I had trouble with the local variable current being in the A_Star function
+// and I wanted to use it in this funtion so I just recreated it and all it's changes, even thought its very ineficient.
 if (!nosolution) {
     let winner = 0;
     for (let i = 0; i < openSet.length; i ++) {
@@ -213,7 +231,8 @@ if (!nosolution) {
     let current = openSet[winner];
 
     if (current === end) {
-      //find the path
+      // this finds the path, by going from the end cell throught all
+      // the cells it came from to get back to the start cell and get the final path the algorithim calculated.
       path = [];
       let temp = current;
       path.push(temp);
@@ -225,6 +244,10 @@ if (!nosolution) {
       console.log("DONE");
     }
 
+    // I dont know why but when I dont have this chunk of code wich is just a duplicate of the one above it
+    // the algoritim doesint stop where it should and the it tries to calculate the path for one more frame
+    // wich makes the end path wrong, but with this code here it works fine so I left it even thought I 
+    // know this shoundint really be nessessairy.
     path = [];
     let temp = current;
     path.push(temp);
@@ -234,6 +257,7 @@ if (!nosolution) {
     }
 }
 
+// makes the cells that the path takes be blue
   for (let i = 0; i < path.length; i ++) {
     path[i].show(color(0, 0, 255));
   }
